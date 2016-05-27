617f633c5649775fe7235b0178118a5a7758fe24
fitellez <dante_92_007@hotmail.com>
2016-05-26 22:49:44 -0500

    Juego terminado
    **Incluye ayuda y actualizacion en la pagina de Greenfoot

--- Juego tanque_Objetos/Portada.java
+++ Juego tanque_Objetos/Portada.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Portada extends World
 {
     private Nivel1 level1;
+    private Ayuda help;
     /**
      * Constructor for objects of class Portada.
      * 
@@ -18,6 +19,8 @@ public class Portada extends World
         super(650, 500, 1);
         setBackground("portada.png");
         level1=new Nivel1();
+        help= new Ayuda();
+        
     }
 
     public void act()
@@ -26,5 +29,11 @@ public class Portada extends World
         {
             Greenfoot.setWorld(level1); 
         }
+        if(Greenfoot.isKeyDown("h"))
+        {
+            removeObjects(getObjects(null));
+            setBackground("Ayuda.jpg");
+        }
+          
     }
-}
+    }
